f30247e733fca0a4a26e80a6092bb14a017a3b98
mango906 <minkb01@gmail.com>
2020-02-18 00:17:52 +0900

    Revert "feat: Input 축"
    This reverts commit c6c91633afd35284796cc6a9f7af4803e5d91deb.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Input } from "./components/Input";
 import { Header } from "./components/Header";
 import { DisplayBanner } from "./components/DisplayBanner";
 
@@ -9,20 +8,14 @@ const App = () => {
   return (
     <Container>
       <Header />
-      <Content>
-        <Input placeholder={"width(px)"} />
-        <Input placeholder={"height(px)"} />
-        <DisplayBanner style={{ marginTop: 24 }} />
-      </Content>
+      <DisplayBanner></DisplayBanner>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled("div")`
   min-height: 100vh;
   background-color: #000;
 `;
-
-const Content = styled.div``;
 
 export default App;
