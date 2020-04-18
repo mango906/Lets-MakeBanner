54d075f77d025c056206af9964374e8980d51af9
mango906 <minkb01@gmail.com>
2020-04-18 16:22:04 +0900

    feat: canvas 요소 마우스로 이동 이벤트 추가

--- src/components/DisplayBanner/index.tsx
+++ src/components/DisplayBanner/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { WHITE } from '../../utils/color';
@@ -13,8 +13,17 @@ interface Props {
 
 export const DisplayBanner = (props: Props) => {
   const { width, height, text, backgroundColor, style } = props;
+  const [drawable, setDrawable] = useState(false);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const initDraw = useCallback(() => {
+    setDrawable(true);
+  }, []);
+
+  const finishDraw = useCallback(() => {
+    setDrawable(false);
+  }, []);
 
   useEffect(() => {
     const ctx = canvasRef.current!.getContext("2d");
@@ -28,7 +37,39 @@ export const DisplayBanner = (props: Props) => {
     ctx!.fillText(text, width / 2, height / 2);
   }, [width, height, text]);
 
-  return <Container ref={canvasRef} style={style} width={width} height={height} backgroundColor={backgroundColor} />;
+  const handleMove = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      if (!drawable) return;
+
+      const ctx = canvasRef.current!.getContext("2d");
+
+      ctx!.clearRect(0, 0, width, height);
+
+      ctx!.font = "20px SF Pro";
+      ctx!.textAlign = "center";
+      ctx!.fillStyle = WHITE;
+
+      const x = e.pageX - canvasRef.current!.offsetLeft;
+      const y = e.pageY - canvasRef.current!.offsetTop;
+
+      ctx!.fillText(text, x, y);
+    },
+    [text, drawable]
+  );
+
+  return (
+    <Container
+      ref={canvasRef}
+      style={style}
+      width={width}
+      height={height}
+      backgroundColor={backgroundColor}
+      onMouseDown={initDraw}
+      onMouseMove={handleMove}
+      onMouseUp={finishDraw}
+      onMouseOut={finishDraw}
+    />
+  );
 };
 
 const Container = styled.canvas<{
