89f0db0528593f35a30eea58e392d009ea96f797
mango906 <minkb01@gmail.com>
2020-04-18 16:56:27 +0900

    refactor: Button 시멘틱 태그에 맞춰서 변경

--- src/components/ColorButton/index.tsx
+++ src/components/ColorButton/index.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 interface Props {
   color: string;
   onClick: () => void;
+  icon?: any;
 }
 
 export const ColorButton = (props: Props) => {
-  const { color, onClick } = props;
+  const { color, onClick, icon } = props;
 
-  return <Wrapper color={color} onClick={onClick} />;
+  return (
+    <Wrapper color={color} onClick={onClick}>
+      {icon && icon}
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled("div")<{ color: string }>`
+const Wrapper = styled("button")<{ color: string }>`
   width: 40px;
   height: 40px;
   background-color: ${(props) => props.color};
